5f6d32f9d5396979a43601bc3210ba03b90d3516
MikuGeek <56752306+MikuGeek@users.noreply.github.com>
2025-04-19 14:26:58 +0800

    style: Lint nvim config

--- dot_config/nvim/lua/plugins/language.lua
+++ dot_config/nvim/lua/plugins/language.lua
@@ -1,16 +1,17 @@
 return {
-    { "MeanderingProgrammer/render-markdown.nvim",
-        config = function()
-            require('blink.cmp').setup({
-                sources = {
-                    completion = {
-                        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'markdown' },
-                    },
-                    providers = {
-                        markdown = { name = 'RenderMarkdown', module = 'render-markdown.integ.blink' },
-                    },
-                },
-            })
-        end
-    },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    config = function()
+      require("blink.cmp").setup({
+        sources = {
+          completion = {
+            enabled_providers = { "lsp", "path", "snippets", "buffer", "markdown" },
+          },
+          providers = {
+            markdown = { name = "RenderMarkdown", module = "render-markdown.integ.blink" },
+          },
+        },
+      })
+    end,
+  },
 }
